4d91dbd8c22924711196b02403f171fb63b007a9
Aron Hegyi <ahegyi@gmail.com>
2013-05-07 16:05:39 -0700

    Add automatic geocoding to Query objects after validation

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,3 +1,18 @@
 class Query < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :id, :created_at, :updated_at, :input, :named_place, :radius, :lat, :lng
+
+  validates :input, :presence => true
+
+  geocoded_by :input, :latitude  => :lat, :longitude => :lng do |query, results|
+    if geo = results.first
+      # only tested with ESRI as geocoding service
+      query.named_place = geo.data["locations"].first["name"]
+      # have to do this manually, I guess
+      query.lat = geo.latitude
+      query.lng = geo.longitude
+    end
+  end
+
+  after_validation :geocode # auto-fetch coordinates
+
 end
