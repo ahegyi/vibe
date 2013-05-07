502b3e051f3c9a45de5ba422c91ddde4e7951b7c
Aron Hegyi <ahegyi@gmail.com>
2013-05-06 20:15:24 -0700

    Added index route and view to vibe controller

--- app/controllers/vibe_controller.rb
+++ app/controllers/vibe_controller.rb
@@ -1,2 +1,13 @@
 class VibeController < ApplicationController
+
+  def index
+    @images = {}
+
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @images }
+    end
+  end
+
 end
