960bfae1e71e002ec34d886df6588651665ac275
Aron Hegyi <ahegyi@gmail.com>
2013-05-06 17:07:33 -0700

    First commit of our friend, vibe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vibe::Application.config.secret_token = 'a40ae6637e7bb82b7d750878b2d7be6972e59fe1a5e4595cf75e1466bb6e77d74aed4ac7b16e1111464fd4590b43858a13e91aaec957f6211f16a96deb176804'
