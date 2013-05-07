42a87737745175e86a6629e1f3a54fe8668a7817
Aron Hegyi <ahegyi@gmail.com>
2013-05-07 10:41:36 -0700

    Add some tests for the get_point geocoding helper

--- test/unit/helpers/vibe_helper_test.rb
+++ test/unit/helpers/vibe_helper_test.rb
@@ -1,4 +1,17 @@
 require 'test_helper'
 
 class VibeHelperTest < ActionView::TestCase
+
+  test "san francisco is san francisco" do
+    point = get_point("san francisco")
+
+    assert_equal [37.77896, -122.4192], point
+  end
+
+  test "incomprehensible text is an empty array" do
+    point = get_point("skdfjhsldkjfhsdkjf")
+
+    assert_equal [], point
+  end
+
 end
