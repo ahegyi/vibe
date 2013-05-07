316113c1cdca32bbf2cbf0f2fb627bfdc806b4f8
Matt Cano <cano.matt@gmail.com>
2013-05-06 23:30:34 -0700

    adding foursquare2 gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def fs_client
+    return Foursquare2::Client.new(:client_id => ENV['UYVKTX2TB32OSSWQOE5LII32C35AGHEJUCTLMSBPV0RUGNV'], :client_secret => ENV['UYVKTX2TB32OSSWQOE5LII32C35AGHEJUCTLMSBPV0RUGNV'])
+  end
+
 end
--- app/helpers/foursquare_helper.rb
+++ app/helpers/foursquare_helper.rb
@@ -1,5 +1,11 @@
 module FourSquareHelper
 
+  def fs_search_ll(lat, lng)
 
-  
+  end
+
+  def fs_search_place(place)
+
+  end
+
 end
