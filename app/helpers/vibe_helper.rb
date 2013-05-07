14ed84029d0d42bf07115a507ee10106ad5b947b
Aron Hegyi <ahegyi@gmail.com>
2013-05-07 10:42:33 -0700

    Added get_point helper function

--- app/helpers/vibe_helper.rb
+++ app/helpers/vibe_helper.rb
@@ -1,2 +1,21 @@
+require 'open-uri'
+require 'uri'
+
 module VibeHelper
+
+  def get_point(input)
+    geocode_base_uri = "http://geocoding.cloudmade.com/" + ENV["CLOUDMADE_API_KEY"] + "/geocoding/v2/find.js"
+
+    results = open(geocode_base_uri + "?query=" + URI.escape(input))
+
+    centroid = []
+
+    results_hash = JSON.load(results.read)
+    if !results_hash.nil? && !results_hash.empty? && results_hash.keys.include?("found")
+      centroid = results_hash["features"][0]["centroid"]["coordinates"]
+    end
+
+    return centroid
+  end
+
 end
