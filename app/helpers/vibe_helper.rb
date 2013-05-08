949a3584b9bd40bc08d9d3e1425d9bd317bc4034
Matt Cano <cano.matt@gmail.com>
2013-05-07 19:21:18 -0700

    error in ENV var name. fixed.

--- app/helpers/vibe_helper.rb
+++ app/helpers/vibe_helper.rb
@@ -4,7 +4,7 @@ module VibeHelper
   meters_in_km = 1000
 
   def foursquare_ll(ll)
-    client = Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_SECRET_ID'], :api_version => '20130505')
+    client = Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'], :api_version => '20130505')
     venues  = client.trending_venues(ll, {:limit => 10, :radius => 5000}).venues
     fs_entities = []
     s_latlng = ll.split(',').map{|i| i.to_f}
