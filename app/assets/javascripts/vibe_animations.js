22686b76d1d111204b325dd28a620e25b1728c1b
Rob Colby <rcolby@me.com>
2013-05-09 19:36:01 -0700

    editing index animations. changed searchnav duration and put tiles into a complete function

--- app/assets/javascripts/vibe_animations.js
+++ app/assets/javascripts/vibe_animations.js
@@ -97,7 +97,7 @@ function getMovementSpeed(interestingness) {
   }
 }
 
-$(window).load(function() {
+$(window).ready(function() {
   var locationBox = $('#searchbox');
   var searchComp = $('#searchcomponent');
   var nav = $('#sideNav');
@@ -118,46 +118,47 @@ $(window).load(function() {
 
   //Search bar animation
   $('#go').on('click', function(){
-            $(nav).animate({
-                  width : '100%',
-                  height : '45px',
-                  fontSize : '50px',
-                  marginTop : '-24px',
-                  marginLeft : '0',
-                  paddingLeft : '70px',
-                  paddingBottom : '20px',
-                  borderRadius : '',
-                  zIndex: '1'
-                }, {
-                  duration: 350,
-                  specialEasing: {
-                    sarah : ".71,.01,.4,.99"
-                  },
-                  complete : function(){
-                    $(locationBox).animate({
-                        height: '40px',
-                        fontSize : '15px',
-                        marginTop : '-70px',
-                        marginLeft : '500px',
-                        marginRight : '0',
-                        zIndex: '2'
-                      }, {
-                        duration: 150,
-                        specialEasing: {
-                        sarah : "0,0,1,1"
-                        }
+    $(nav).animate({
+          width : '100%',
+          height : '45px',
+          fontSize : '50px',
+          marginTop : '-24px',
+          marginLeft : '0',
+          paddingLeft : '70px',
+          paddingBottom : '20px',
+          borderRadius : '',
+          zIndex: '1'
+        }, {
+          duration: 1750,
+          specialEasing: {
+            sarah : ".71,.01,.4,.99"
+        }
+      }
+    );
+    $(locationBox).animate({
+          height: '40px',
+          fontSize : '15px',
+          marginTop : '-70px',
+          marginLeft : '500px',
+          marginRight : '0',
+          zIndex: '2'
+      }, {
+          duration: 1550,
+          specialEasing: {
+          sarah : "0,0,1,1"
+        },
+        complete: function(){
+           $('.tile').show('scale');
+              $.each(tiles, function(index, tile) {
+                tile.move(tile.currentLeft());
             });
-              locationBox.css('position', 'absolute');
-              $('#go').addClass('hidden');
-            }
-        });
-      nav.css('textAlign', 'left');
-      $('#map-canvas').css('opacity', '.5');
-
-    $('.tile').show('scale');
-      $.each(tiles, function(index, tile) {
-        tile.move(tile.currentLeft());
-    });
+          }
+        }
+    );
+    locationBox.css('position', 'absolute');
+    $('#go').addClass('hidden');
+    nav.css('textAlign', 'left');
+    $('#map-canvas').css('opacity', '.5');
   });
     $('body').on('click', '.tile', function(event) {
     $('body').unbind('click');
@@ -172,6 +173,5 @@ $(window).load(function() {
       });
     });
   });
-
 
 });
