d57d7e4b1be87e05648e5092f8cb081f4ee24878
Aron Hegyi <ahegyi@gmail.com>
2013-05-09 21:18:21 -0700

    Add entities and geocode route to render jsons

--- app/controllers/vibe_controller.rb
+++ app/controllers/vibe_controller.rb
@@ -12,6 +12,43 @@ class VibeController < ApplicationController
     end
   end
 
+  def entities_for_query
+    query = Query.new
+    @entities = []
+
+    start_time = Time.now
+
+    query.input = params[:query]
+    query.geocode
+
+    if !query.lat.nil? && !query.lng.nil?
+      @entities += foursquare_ll(query.lat, query.lng)
+      @entities += twitter_ll(query.lat, query.lng)
+      @entities += instagram_ll(query.lat, query.lng)
+    end
+
+    # sort by interestingness descending
+    @entities.sort! { |a,b| a.interestingness <=> b.interestingness }.reverse!
+
+    puts "Took #{Time.now - start_time} seconds"
+
+    respond_to do |format|
+      format.json { render json: @entities }
+    end
+
+  end
+
+  # returns a json object with the query and the lat/long as an array 'coordinates'
+  def geocode
+    query = params[:query]
+    coordinates = Geocoder.coordinates(query)
+    @place = { "query" => query, "coordinates" => coordinates }
+    respond_to do |format|
+      format.json { render json: @place }
+    end
+  end
+
+
   def foursquare
 
     @place = "Washington D.C."
@@ -46,7 +83,7 @@ class VibeController < ApplicationController
     @place = "San Francisco, CA"
 
     coordinates = Geocoder.coordinates(@place)
-   
+
     @flickr_arr = flickr_ll(coordinates[0], coordinates[1])
 
     respond_to do |format|
